19034e2d1421d4c8e147b4a0bffd0a7ecbc3ac3c
Denny Hollick <dennyhollick@gmail.com>
2017-07-26 14:08:31 -0700

    Added websocket functionality to send new message to server

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
+
+function sendMessageToServer(newMessage) {
+  chattyWebSocket.send(JSON.stringify(newMessage));
+}
+
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -31,21 +38,17 @@ class App extends Component {
       const newMessages = this.state.messages.concat(newMessage);
       this.setState({messages: newMessages});
       event.target.value = '';
+      this.chattyWebSocket.send(JSON.stringify(newMessage));   
     }
   }
 
   componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
-}
+    this.chattyWebSocket = new WebSocket("ws://localhost:3001");
+    console.log("componentDidMount <App />");
+    chattyWebSocket.onopen = function (event) {
+      console.log("The connection is open"); 
+    };
+  }
 
 
   render() {
