a698d3ba7e0f64471c30ea82f2c4eefd7e911240
Denny Hollick <dennyhollick@gmail.com>
2017-07-26 12:49:14 -0700

    Added state, enterkeypress event, and return of page render.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {
+        name: 'Bob'
+      }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?'
+        }, {
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    }
+    this.enterKeyPress = this.enterKeyPress.bind(this);
+  }
+
+  enterKeyPress(event) {
+    if (event.key == 'Enter') {
+      let newMessage = {
+        username: this.state.currentUser.name,
+        content: event.target.value
+      };
+      const newMessages = this.state.messages.concat(newMessage);
+      this.setState({messages: newMessages});
+      event.target.value = '';
+    }
+  }
+
+  componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+  }, 3000);
+}
+
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList messages={this.state.messages}/>
+        <ChatBar user={this.state.currentUser} onMessage={this.enterKeyPress}></ChatBar>
+      </div>
     );
   }
+
 }
 export default App;
