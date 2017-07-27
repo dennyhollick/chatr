0c5da7a262864acea333d8950dfc5fe273b0cfe5
Denny Hollick <dennyhollick@gmail.com>
2017-07-27 14:30:05 -0700

    Added navbar module

---
+++ src/NavBar.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let numUsers = this.props.numUsers;
+    return (
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+          <span className="navbar-counter">Total Connected Users: {numUsers}</span>
+        </nav>
+   );
+  }
+}
+export default NavBar;
