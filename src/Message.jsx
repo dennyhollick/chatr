309a3cb305cb0509f55551eea9984d88fb9c58f2
Denny Hollick <dennyhollick@gmail.com>
2017-07-27 20:07:34 -0700

    Added option to add Gifs to messages

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
 
+//Set min length
+
+const gifChek =  function checkIfGif (content) {
+  let type = content.split('.').pop();
+  console.log(type)
+  let website = content.split('.')
+  console.log(website[1])
+  if (type == 'gif' && website[1] == 'giphy') {
+    return true; 
+  } else {
+    return false;
+  }
+}
+
+
 class Message extends Component {
+
 
   render() {
     const { username, content, colour} = this.props.message;
-    if (username) {
+    
+    if (content && gifChek(content)) {
+      return (
+         <div className="message">
+          <span className="message-username" style={{color: colour}}>{username}</span>
+          <span className="message-content"><img src={content}/></span>
+        </div>
+      )
+    }
+    else if (username) {
       return (
         <div className="message">
           <span className="message-username" style={{color: colour}}>{username}</span>
