99b1c52abe5e7928e293e4485f97c80073bcce40
Denny Hollick <dennyhollick@gmail.com>
2017-07-27 21:55:43 -0700

    Linted, indentation fix

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 
-const gifChek =  function checkIfGif (content) {
+const gifChek = function checkIfGif(content) {
   let type = content.split('.').pop();
   let website = content.split('.')
   if (type == 'gif' && website[1] == 'giphy') {
-    return true; 
+    return true;
   } else {
     return false;
   }
@@ -12,22 +12,21 @@ const gifChek =  function checkIfGif (content) {
 
 class Message extends Component {
 
-
   render() {
     const { username, content, colour } = this.props.message;
-    
+
     if (content && gifChek(content)) {
       return (
-         <div className="message">
-          <span className="message-username" style={{color: colour}}>{username}</span>
-          <span className="message-content"><img src={content}/></span>
+        <div className="message">
+          <span className="message-username" style={{ color: colour }}>{ username }</span>
+          <span className="message-content"><img src={ content }/></span>
         </div>
       )
-    }
-    else if (username) {
+    } else if (username) {
       return (
         <div className="message">
-          <span className="message-username" style={{color: colour}}>{username}</span>
+          <span
+            className="message-username" style={{ color: colour}}>{username}</span>
           <span className="message-content">{content}</span>
         </div>
       )
